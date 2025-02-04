80db82d037064bc1d1168169b725f3d7321cf240
cavejon <tehviperry@gmail.com>
2025-02-04 15:05:47 -0300

    New: Criando casos de teste para checkout

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -7,6 +7,38 @@ describe('Usuário logado na página de dashboard', function () {
     });
 
     context('Checkout do item que estava no carrinho', function () {
+
+        it('Adiciona um item ao carrinho e valida campos vazios com mensagem de erro', () => {
+            cy.get('@el').then((el) => {
+                cy.get(el.btn_add).click();
+                cy.get('.shopping_cart_badge').should('contain', '1');
+                cy.get('.shopping_cart_link').click();
+
+                // Clique do botão e avanço
+                cy.get(el.btn_checkout).click();
+                cy.location('pathname').should('eq', '/checkout-step-one.html');
+
+                // Clique com campos vazios e valida erro
+                cy.get(el.btn_continue).click();
+                cy.get('.error-message-container').should('be.visible').and('contain', 'Error: First Name is required');
+
+                // Preenche primeiro nome e tenta continuar
+                cy.get('[data-test="firstName"]').type("Pedro Lucas");
+                cy.get(el.btn_continue).click();
+                cy.get('.error-message-container').should('be.visible').and('contain', 'Error: Last Name is required');
+
+                // Preenche último nome e tenta continuar
+                cy.get('[data-test="lastName"]').type("Cavejon - Teste");
+                cy.get(el.btn_continue).click();
+                cy.get('.error-message-container').should('be.visible').and('contain', 'Error: Postal Code is required');
+
+                // Insere os dados completos
+                cy.get('[data-test="postalCode"]').type("89231-552");
+                cy.get(el.btn_continue).click();
+            });
+        });
+
+
         it('Adiciona um item ao carrinho e valida os detalhes', function () {
             cy.get('@el').then((el) => { // Usa @el para acessar os seletores carregados
                 cy.get(el.btn_add).click();
