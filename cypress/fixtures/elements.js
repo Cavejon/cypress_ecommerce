2c550b171c6c4e1aa9dc6544e1a134014bc4eba3
cavejon <tehviperry@gmail.com>
2025-02-04 11:06:40 -0300

    New: Criando Objetos para serem usados

---
+++ cypress/fixtures/elements.js
@@ -0,0 +1,13 @@
+exports.el ={
+    title:".inventory_item_name",
+    description:".inventory_item_desc",
+    price:".inventory_item_price",
+    btn_add:"#add-to-cart-sauce-labs-bike-light",
+    btn_remove:"#remove-sauce-labs-backpack",
+    btn_checkout:"#checkout",
+    btn_continue_shopping:"#continue-shopping",
+    badge_cart:".shopping_cart_link",
+
+
+
+}
